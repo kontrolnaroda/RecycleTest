136dfe364e688a34e1fe27340413547312e47283
Yuri Pavlov <kontrolnaroda@mail.ru>
2021-06-20 20:22:01 +0300

    finishing of clock

--- StopWhatch/app/src/main/java/com/example/stopwhatch/MainActivity.java
+++ StopWhatch/app/src/main/java/com/example/stopwhatch/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.stopwhatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,31 +11,49 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 private int seconds = 0;
-private boolean isRunnning = false;
+private boolean isRunning = false;
 private TextView textViewTimer;
+private  boolean wasRunning = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textViewTimer = findViewById(R.id.TextViewTimer);
+        runTimer();
     }
 
     public void onclickStartTimer(View view) {
-        isRunnning = true;
+        isRunning = true;
+wasRunning = true;
     }
 
     public void onclickStopTimer(View view) {
-        isRunnning = false;
+        if (wasRunning) {
+            isRunning = false;
+            wasRunning = false;
+        } else { isRunning = true; wasRunning = true;}
     }
 
     public void onclickRestartTimer(View view) {
-        isRunnning = false;
+        isRunning = false;wasRunning = false;
         seconds = 0;
     }
     private  void runTimer(){
-        int hours = seconds / 3600;
-        int minutes = (seconds % 3600) /60;
-        int secs = seconds /  60;
-        String.format(Locale.getDefault(), %2d:%2)
+      final  Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) /60;
+                int secs = seconds %  60;
+                String time = String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, secs);
+                textViewTimer.setText(time);
+                if (isRunning) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+
     }
 }
